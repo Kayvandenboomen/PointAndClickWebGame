4259bd70ab16057e1ab1d355ab7e41726ccb3250
Kayvandenboomen <144001732+Kayvandenboomen@users.noreply.github.com>
2024-02-08 12:51:07 +0100

    Lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-document.getElementById("MainTitle").innerText = "FUCK YOU LITTLE NIGGER";
+document.getElementById("MainTitle").innerText = "Point and Click adventure";
+
+//game window reference
+const gamewindow = document.getElementById("gameWindow");
+
+//main character reference
+const mainCharacter = document.getElementById("mainCharacter")
+const offsetCharacter = 16;
+
+//foreground items
+const door1 = document.getElementById("door1")
+
+gamewindow.onclick = function (e) {
+    var rect = gamewindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    console.log(e.target.id);
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
+
+    switch (e.target.id) {
+        case "door1":
+            mainCharacter.style.backgroundColor = "#964B00"
+            door1.style.opacity = 0.5;
+            break
+
+        default:
+            mainCharacter.style.backgroundColor = "#ff0000"
+            door1.style.opacity = 1;
+            break;
+
+    }
+
+}
